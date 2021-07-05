64a62d0226acd52f12bb1f511ce4d8f7d5832ae5
kyyy1 <81406968+kyyy1@users.noreply.github.com>
2021-07-05 14:32:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,61 +23,6 @@ bot.on("message", async message => {
 	if(message.author.bot) return undefined;
 	if(message.channel.type === 'dm') return;
  
- if(message.content === 'hi') {
-    message.channel.send('Hi Juga<a:sNLP_Yeayy:595130531830562856>')
-
-  }
-
-  if(message.content === 'Hi') {
-    message.channel.send('Hi Juga<a:sNLP_Yeayy:595130531830562856>')
-
-  }
-
-  if(message.content === 'hallo') {
-    message.channel.send('Hallo Juga<a:sNLP_Yeayy:595130531830562856>')
-
-  }
-
-  if(message.content === 'halo') {
-    message.channel.send('Hallo Juga<a:sNLP_Yeayy:595130531830562856>')
-
-  }
-  
-  if(message.content === 'welcome') {
-  message.channel.send('<a:yNLP_welcome6:718324057027903558><a:yNLP_welcome5:718324079639396404>')
-    
-   
- }
-  
-  if(message.content === 'Hai') {
-  message.channel.send('Hai Juga<a:sNLP_Yeayy:595130531830562856>')
-  
-  }
-  
-  if(message.content === 'hai') {
-  message.channel.send('Hai Juga<a:sNLP_Yeayy:595130531830562856>')
-    
-  }
-  
-  if(message.content === 'Pagi') {
-  message.channel.send('Selamat pagi juga, semangat menjalani hari ini<a:1NLP_1Love:721780910567194766>')
-    
-  }
-    
-  if(message.content === 'Assalamualaikum') {
-  message.channel.send('Waalaikumsalam')
-    
-  }
-  
-  if(message.content === 'assalamualaikum') {
-  message.channel.send('Waalaikumsalam')
-    
-  }
-  
-    if(message.content === 'Halo') {
-    message.channel.send('Hallo Juga<a:sNLP_Yeayy:595130531830562856>')
-      
-  }
   
   if(message.content === 'p') {
     message.channel.send('Ha?')
@@ -87,89 +32,22 @@ bot.on("message", async message => {
     if(message.content === 'P') {
     message.channel.send('Ha?')
 
-   }
-  
-  if(message.content === 'pagi') {
-  message.channel.send('Selamat pagi juga, semangat menjalani hari ini<a:1NLP_1Love:721780910567194766>')
-  
   }
-  
-  if(message.content === 'malam') {
-  message.channel.send('Malam~ jangan begadang ya!')
-
-  }
-
-if(message.content === 'Malam') {
-  message.channel.send('Malam~ jangan begadang ya!')
-
-}
-  
-  if(message.content === 'Sepi') {
-  message.channel.send('Ramein dong')
-    
-  }
-  
-  if(message.content === 'sepi') {
-  message.channel.send('Ramein dong')
-    
-  }
-  
-  if(message.content === 'Siang') {
-  message.channel.send('Masih semangat ga nih? Jangan lupa makan siang ya')
-
-  }
-
-if(message.content === 'siang') {
-  message.channel.send('Masih semangat ga nih? Jangan lupa makan siang ya')
-  
- }
-
-  if(message.content === 'sayang') {
-  message.channel.send('Apa sayang?<a:pNLP_YuliMwah:722075893958180864>')
-
-  }
-
- if(message.content === 'Sayang') {
-  message.channel.send('Apa sayang?<a:pNLP_YuliMwah:722075893958180864>')
-  
- }
-  
-  if(message.content === 'Welcome') {
-  message.channel.send('<a:yNLP_welcome6:718324057027903558><a:yNLP_welcome5:718324079639396404>')
-    
-  }
-  
+	    
   if(message.content === 'hm') {
   message.channel.send('<:hm:817019804874440725>')
     
-  }
-  
-  
-  if(message.content === 'alin') {
-  message.channel.send('<@470281222317146118> minta duit')
-    
-  }
+  }	  
   
   if(message.content === '<@461096907624677376>') {
   message.channel.send('apa')
     
   }
-    
-    
-  if(message.content === 'mwah') {
-  message.channel.send('mwah<a:pNLP_Kiss:778873499619426315>')
-    
-     }
-  
-    if(message.content === 'Mwah') {
-  message.channel.send('mwah<a:pNLP_Kiss:778873499619426315>')
-      
-    }
   
     if(message.content === 'Hm') {
   message.channel.send('<:hm:817019804874440725>')
       
-    }
+  }
 
 });
 
